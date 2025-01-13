309aa0e0da400c5098cb4d51e196200d6a9d3a28
Zion Obcena <zmobcena@gmail.com>
2025-01-13 22:14:05 +0800

    Added code for drawing octagon

--- GDGRAP/main.cpp
+++ GDGRAP/main.cpp
@@ -1,15 +1,21 @@
 #include <GLFW/glfw3.h>
+#include <cmath>
+
+const float PI = 3.14159265359f;
 
 int main(void)
 {
     GLFWwindow* window;
+
+    float fRadius = 1.f;
+    float fYOffset = 0.07f;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 640, "Zion Miguel Obcena", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -25,10 +31,17 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+        glBegin(GL_POLYGON);
+        for (int i = 0; i < 8; ++i)
+        {
+            float fAngle = (PI / 4) * i;
+            float fNewAngle = fAngle - (PI / 8);
+
+            float x = fRadius * cos(fNewAngle);
+            float y = fRadius * sin(fNewAngle) + fYOffset;
+
+            glVertex2f(x, y);
+        }
         glEnd();
 
         /* Swap front and back buffers */
